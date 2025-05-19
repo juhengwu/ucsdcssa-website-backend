79365df2254fe5832261ba9967888997bbbfeea8
Jerry Gong <97704595+JGong7@users.noreply.github.com>
2025-05-19 10:38:08 -0700

    修改照片API尝试

--- blueprints/department_blueprint.py
+++ blueprints/department_blueprint.py
@@ -10,38 +10,38 @@ api = Api(department_bp)
 
 
 class Department(Resource):
-    def get(self, department_name):
-        json_url = os.path.join(SITE_ROOT, 'assets', 'department.json')
+    def get(self, department_name, year):
+        json_url = os.path.join(SITE_ROOT, year + '-assets', 'department.json')
         with open(json_url, encoding="utf8") as f:
             department = json.load(f)
         return department[department_name]
 
 
 class Member(Resource):
-    def get(self, department_name, member_name):
-        json_url = os.path.join(SITE_ROOT, 'assets', 'department.json')
+    def get(self, department_name, member_name, year):
+        json_url = os.path.join(SITE_ROOT, year +'-assets', 'department.json')
         with open(json_url, encoding="utf8") as f:
             department = json.load(f)
         return send_file(
             os.path.join(
                 SITE_ROOT,
-                "assets/members-photos",
+                year + "-assets/members-photos",
                 department[department_name]["members"][member_name]["photoSrc"]
             )
         )
 
 
 class GroupPhoto(Resource):
-    def get(self, department_name):
+    def get(self, department_name, year):
         return send_file(
             os.path.join(
                 SITE_ROOT,
-                "assets/departments-group-photos",
+                year + "-assets/departments-group-photos",
                 "{}合照.jpg".format(department_name)
             )
         )
 
 
-api.add_resource(Department, "/department/<string:department_name>")
-api.add_resource(Member, "/member/<string:department_name>/<string:member_name>")
-api.add_resource(GroupPhoto, "/group-photo/<string:department_name>")
+api.add_resource(Department, "/department/<string:department_name>/<string:year>")
+api.add_resource(Member, "/member/<string:department_name>/<string:member_name>/<string:year>")
+api.add_resource(GroupPhoto, "/group-photo/<string:department_name>/<string:year>")
